7dae4a2ac6e16f5778d47edf79ae5878eba3e7c4
wowblast <jfrt142012@hotmail.com>
2020-04-26 11:13:40 -0400

    added

--- Program.cs
+++ Program.cs
@@ -81,5 +81,111 @@ namespace HashTableProject
                 }              
             }
         }
+        public int FuncionCI(int ci)
+        {
+            return ci % cantidad;
+        }
+        public int FuncionName(string name)
+        {
+            return name.Length % cantidad;
+        }
+        public void SearchByCI(int ci)
+        {
+            int i = 0; //Contador de intentos
+            int pLlave = ci;
+            int indice = 0;
+            bool encontrado = false;
+
+            //calculamos el indice
+            indice = FuncionCI(pLlave);
+            int primerIndice = indice;
+            if (tablaCI[indice] == null)
+            {
+                Console.WriteLine("no se encontro el valor de clave {0}", pLlave);
+            }
+            else
+            {
+
+                BinarySearch(pLlave, 0, getLength(tablaCI[indice]) - 1, tablaCI[indice]); 
+            }
+
+        }
+       
+        public void BinarySearch( int target, int izq, int der,Patient patient)
+        {
+            int medio;
+
+            medio = (int)Math.Floor(((izq + der) * 1.0 / 2.0));
+            if (der - izq > 1)
+            {
+                int CImedio = getPatient(patient, medio).CI;
+                if (CImedio == target)
+                {
+                    Console.WriteLine("numero encontrado {0} en pos {1}", CImedio, medio);
+
+                }
+                else if (CImedio < target)
+                {
+                    BinarySearch( target, medio + 1, der,patient);
+                }
+                else if (CImedio > target)
+                {
+                    BinarySearch( target, izq, medio - 1,patient);
+                }
+            }
+            else
+            {
+                int CIizq = getPatient(patient, izq).CI;
+                int CIder = getPatient(patient, der).CI;
+                if (CIizq == target)
+                {
+                    Console.WriteLine("numero encontrado {0} en pos {1}",CIizq, izq);
+
+                }
+                else if (CIder== target)
+                {
+                    Console.WriteLine("numero encontrado {0} en pos {1}", CIder, der);
+
+                }
+                else
+                {
+                    Console.WriteLine("numero no encontrado");
+
+                }
+            }
+
+
+
+        }
+
+        public Patient getPatient (Patient patient, int ind)
+        {
+            int actualInd = 0;
+            if (patient == null)
+            {
+                return null;
+            }
+            else
+            {
+                while (actualInd!=ind)
+                {                   
+                    patient = patient.Next;
+                    actualInd++;
+                }
+                return patient;
+            }
+        }
+
+        public int getLength(Patient patient)
+        {
+            int lenght = 0;
+            while (patient != null)
+            {
+                patient = patient.Next;
+                lenght++;
+            }
+            return lenght;
+        }
+
     }
 }
